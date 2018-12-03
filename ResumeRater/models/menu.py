db568cd3f7be8f47a1a7e86066b3a548460c4109
twong40 <twong40@ucsc.edu>
2018-12-02 17:53:00 -0800

    New menubar

--- ResumeRater/models/menu.py
+++ ResumeRater/models/menu.py
@@ -49,12 +49,12 @@ def _():
     # useful links to internal and external resources
     # ------------------------------------------------------------------------------------------------------------------
     response.menu += [
-        (T('My Sites'), False, URL('admin', 'default', 'site')),
-        (T('Sample Menu 1'), False, '#', [
-            (T('Entry1'), False, URL('default', 'entry1')),
-            (T('Entry2'), False, URL('default', 'entry2')),
-        ]),
-        (T('Sample Menu 2'), False, URL('default', 'index'))
+        (T('Rate a Resume'), False, URL('admin', 'default', 'site')),
+        # (T('Sample Menu 1'), False, '#', [
+        #     (T('Entry1'), False, URL('default', 'entry1')),
+        #     (T('Entry2'), False, URL('default', 'entry2')),
+        # ]),
+        (T('Reviews'), False, URL('default', 'index'))
     ]
 
 
