69fb483a240db60560c21efbba293c5b7dc0e6b4
twong40 <twong40@ucsc.edu>
2018-12-07 01:15:28 -0800

    still not working with firebase

--- static/js/default_hub.js
+++ static/js/default_hub.js
@@ -1,6 +1,27 @@
-// This is the js for the default/index.html view.
-var app = function() {
+// This is the js for the default/hub.html view.
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCxdoH5XmTKOXxA37mK_I_gDRi5fkvrXkk",
+    authDomain: "resume-rater.firebaseapp.com",
+    databaseURL: "https://resume-rater.firebaseio.com",
+    projectId: "resume-rater",
+    storageBucket: "resume-rater.appspot.com",
+    messagingSenderId: "220789866717"
+  };
 
+firebase.initializeApp(config);
+var firestore = firebase.firestore();
+//=====================FIREBASE====================
+const docRef = firestore.doc("users/pH4XWMHoEgKvlgcm8r9d");
+const header = document.querySelector("#test");
+const feedback = document.querySelector("#feedbackButton");
+feedback.addEventListener("click", function(){
+  console.log("I am going to save into firestore");
+  docRef.set({
+    name: "Hello"
+  });
+})
+var app = function() {
   var self = {};
 
   Vue.config.silent = false; // show all warnings
@@ -22,6 +43,9 @@ var app = function() {
   self.show_past = function() {
     this.show_past_resumes = !this.show_past_resumes;
   };
+  self.redirect_feedback = function() {
+    redirect(URL('default', 'feedback'));
+  };
   // Complete as needed.
   self.vue = new Vue({
     el: "#vue-div",
@@ -31,7 +55,8 @@ var app = function() {
       show_past_resumes: false,
     },
     methods: {
-      show_past: self.show_past
+      show_past: self.show_past,
+      redirect_feedback : self.redirect_feedback
     }
   });
   return self;
